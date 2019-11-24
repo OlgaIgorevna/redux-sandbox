58dbcd0035f5cc1b2bb1c911d533c75c5c7929fd
Olga Igorevna Pron <olga.pron.kld@gmail.com>
2019-11-24 22:14:32 +0200

    Counter with redux, but WITHOUT React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore, bindActionCreators} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from "./reducer";
+/*import {inc, dec, rnd} from "./actions";*/
+import * as actions from "./actions";
+const store = createStore(reducer);
+const {dispatch} = store;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const bindActionCreator = (funcCreator, dispatch)=>{
+    return (...args)=>{
+        dispatch(funcCreator(...args))
+    }
+};
+
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
+/*const {incDispatch, decDispatch, rndDispatch } = bindActionCreators({
+    incDispatch: inc,
+    decDispatch: dec,
+    rndDispatch: rnd
+}, dispatch);*/
+/*const incDispatch = bindActionCreator(inc, dispatch);
+const decDispatch = ()=>{ dispatch(dec())};
+const rndDispatch = bindActionCreator(rnd, dispatch);*/
+
+document.getElementById("inc")
+    /*.addEventListener("click", incDispatch);*/
+    .addEventListener("click", inc);
+
+document.getElementById("dec")
+    .addEventListener("click", dec);
+
+document.getElementById("rnd")
+    .addEventListener("click", ()=>{
+        const payload = Math.floor(Math.random()*10);
+        rnd(payload);
+    });
+
+const update= ()=>{
+    document.getElementById("counter").innerHTML = store.getState();
+};
+
+store.subscribe(update);
+/*store.subscribe(()=>{
+    console.log("store changed, state: ", store.getState());
+});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});*/
+
+/*let state = reducer(undefined, {});
+state = reducer(state, {type: "INC"});
+console.log(state);*/
